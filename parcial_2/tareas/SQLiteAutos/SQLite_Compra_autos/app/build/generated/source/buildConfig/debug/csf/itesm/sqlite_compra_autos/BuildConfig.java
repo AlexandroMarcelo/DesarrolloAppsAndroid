232e2e0d8668fcc57131e85b661523a42075c1d6
AlexandroMarcelo <alex.fcomarcelo@gmail.com>
2019-11-01 12:11:15 -0600

    partials completed.

---
+++ parcial_2/tareas/SQLiteAutos/SQLite_Compra_autos/app/build/generated/source/buildConfig/debug/csf/itesm/sqlite_compra_autos/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package csf.itesm.sqlite_compra_autos;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "csf.itesm.sqlite_compra_autos";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
